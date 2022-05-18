3fd02bbb5c983d2f0c1bd4250b65a13473a98778
sauricat <linshu1729@protonmail.com>
2022-05-18 22:49:45 +0800

    add nguix

--- package/nix-guix.nix
+++ package/nix-guix.nix
@@ -1,32 +1,26 @@
 { stdenv, pkgs, lib, fetchurl, pkg-config, makeWrapper, zlib, bzip2, guile
 , guilePackages, storeDir ? null, stateDir ? null }:
 
-stdenv.mkDerivation rec {
-  pname = "nixos-guix-symlink";
-  version = "0.1.3";
+runCommand "nixos-guix-symlink" { } 
+  ''
+    mkdir -p $out/bin
+    ln -s ${guix}/bin/guix $out/bin/nguix
 
-  buildInputs = [ guix ];
-  nguix = runCommand "nixos-guix-symlink" { 
-    buildInputs = [ guix ]; } 
-    ''
-      mkdir -p $out/bin
-      ln -s ${guix}/bin/guix $out/bin/nguix
+    mkdir -p $out/share
+    ln -s ${guix}/share/guix $out/share/nguix
 
-      mkdir -p $out/share
-      ln -s ${guix}/share/guix $out/share/nguix
+    mkdir -p $out/share/guile/site
+    ln -s ${guix}/share/guile/site/guix $out/share/guile/site/nguix
 
-      mkdir -p $out/share/guile/site
-      ln -s ${guix}/share/guile/site/guix $out/share/guile/site/nguix
+    mkdir -p $out/share/guile/ccache
+    ln -s ${guix}/share/guile/ccache/guix $out/share/guile/ccache/nguix
 
-      mkdir -p $out/share/guile/ccache
-      ln -s ${guix}/share/guile/ccache/guix $out/share/guile/ccache/nguix
+    mkdir -p $out/libexec
+    ln -s ${guix}/libexec/guix $out/libexec/nguix
 
-      mkdir -p $out/libexec
-      ln -s ${guix}/libexec/guix $out/libexec/nguix
-
-      mkdir -p $out/etc/bash_completion.d
-      ln -s ${guix}/etc/bash_completion.d/guix $out/etc/bash_completion.d/nguix
-    ''; 
+    mkdir -p $out/etc/bash_completion.d
+    ln -s ${guix}/etc/bash_completion.d/guix $out/etc/bash_completion.d/nguix
+  ''; 
 
   meta = with lib; {
     description =
@@ -35,5 +29,3 @@ stdenv.mkDerivation rec {
     license = licenses.gpl3;
     maintainers = with maintainers; [ sauricat ];
     platforms = platforms.linux;
-  };
-}
